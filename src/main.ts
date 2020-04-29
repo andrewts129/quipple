fe2ace91916ecd0ce12a9abc060bf033d552dc93
Andrew Smith <andrewts129@gmail.com>
2020-04-29 14:33:19 -0400

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './root.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const bootstrap = async () => {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
 }
 bootstrap();
