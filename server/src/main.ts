4780ccce4f5db66d267b25a986c71876a0074e05
Andrew Smith <andrewts129@gmail.com>
2020-05-08 18:05:03 -0400

    proper spa serving

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,15 +3,10 @@ require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { RootModule } from './root.module';
-import { join } from 'path';
 import * as cookieParser from 'cookie-parser';
 
 const bootstrap = async () => {
     const app = await NestFactory.create<NestExpressApplication>(RootModule);
-
-    app.useStaticAssets(join(__dirname, '..', '..', 'client', 'build'), {
-        redirect: false
-    });
 
     // TODO still needed?
     app.use(cookieParser());
--- server/src/root.module.ts
+++ server/src/root.module.ts
@@ -4,8 +4,19 @@ import { GameModule } from './game/game.module';
 import { PlayerModule } from './player/player.module';
 import { GameplayModule } from './gameplay/gameplay.module';
 import { InitModule } from './init/init.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-    imports: [AuthModule, GameModule, PlayerModule, GameplayModule, InitModule]
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', '..', 'client', 'build')
+        }),
+        AuthModule,
+        GameModule,
+        PlayerModule,
+        GameplayModule,
+        InitModule
+    ]
 })
 export class RootModule {}
